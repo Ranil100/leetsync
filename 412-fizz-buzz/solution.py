3d958f8051f43e34d8efba84bb5e31ae9ac0e263
Ranil <166100945+Ranil100@users.noreply.github.com>
2026-07-26 22:26:17 +0530

    Time: 3 ms | Memory: 19.7 MB - LeetPush

---
+++ 412-fizz-buzz/solution.py
@@ -0,0 +1,16 @@
+# Fizz Buzz
+# Difficulty: Easy
+# Runtime: 3 ms
+# Memory: 19.7 MB
+# https://leetcode.com/problems/fizz-buzz/
+
+                result.append("Fizz")
+            elif i % 5 == 0:
+                result.append("Buzz")
+            
+            else:
+            elif i % 3 == 0:
+                result.append("FizzBuzz")
+            if i % 3 == 0 and i % 5 == 0:
+        for i in range(1,n+1):
+        
