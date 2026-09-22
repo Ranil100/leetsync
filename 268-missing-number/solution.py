c7ff51e9f2ffcb579b2d814cf00a600c15c0ab9d
Ranil <166100945+Ranil100@users.noreply.github.com>
2026-09-22 19:52:56 +0530

    Time: 3 ms | Memory: 20.5 MB - LeetPush

--- 268-missing-number/solution.py
+++ 268-missing-number/solution.py
@@ -1,16 +1,19 @@
 # Missing Number
 # Difficulty: Easy
 # Runtime: 3 ms
-# Memory: 20.2 MB
+# Memory: 20.5 MB
 # https://leetcode.com/problems/missing-number/
 
-class Solution:
-    def missingNumber(self, nums: List[int]) -> int:
-        n = len(nums)
-        expected_sum = n * (n + 1) // 2
+    def missingNumber(self, nums: list[int]) -> int:
 
-        actual_sum = 0 
+        n= len(nums)
+        s2 = (n * (n + 1)) / 2
+
+        s1=0
         for i in range(n):
-           actual_sum+=nums[i]
-        
-        return expected_sum - actual_sum
+            s1+=nums[i]
+
+        return int(s2-s1)    
+
+
+class Solution:
