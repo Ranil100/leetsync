87b200fbedab73ff7d9c172e6ded0382c47a4d0b
Ranil <166100945+Ranil100@users.noreply.github.com>
2026-09-22 19:51:43 +0530

    Time: 11 ms | Memory: 21.2 MB - LeetPush

---
+++ 977-squares-of-a-sorted-array/solution.py
@@ -0,0 +1,19 @@
+# Squares of a Sorted Array
+# Difficulty: Easy
+# Runtime: 11 ms
+# Memory: 21.2 MB
+# https://leetcode.com/problems/squares-of-a-sorted-array/
+
+class Solution:
+    def sortedSquares(self, nums: list[int]) -> list[int]:
+
+        new_list = []
+
+        for i in range(0,len(nums)):
+
+            new_list.append(nums[i] * nums[i])
+
+        new_list.sort()    
+
+        return new_list
+        
